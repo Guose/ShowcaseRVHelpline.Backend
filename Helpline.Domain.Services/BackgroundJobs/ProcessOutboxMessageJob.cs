ffed15ff458eb885243b39f81d038e9872ac8e5a
Justin Elder <justin.showcase.rvhub@gmail.com>
2024-11-19 20:02:33 -0800

    Added RentalRequest DTO (#62)

---
+++ Helpline.Domain.Services/BackgroundJobs/ProcessOutboxMessageJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+
+namespace Helpline.Domain.Services.BackgroundJobs
+{
+    [DisallowConcurrentExecution]
+    public class ProcessOutboxMessageJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
