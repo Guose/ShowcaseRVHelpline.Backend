81ac0af51bd6ba89935f09268994959034eac16e
Justin Elder <justin.showcase.rvhub@gmail.com>
2024-12-12 11:38:16 -0800

    Delete Helpline.API.UserIdentity directory

--- Helpline.API.UserIdentity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Helpline.API.UserIdentity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
